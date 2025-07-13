55fd2c6cb6042b65258a1796ba55f5a7a40d8e46
Harshvardhan Chauhan <harshvardhan2266@gmail.com>
2025-07-13 17:27:01 +0530

    Conditional Update

---
+++ Conditional.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Conditional {
+    public static void main(String args[]) {
+     Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the number :");
+     int num = sc.nextInt();
+     if(num%2 == 0){
+        System.out.println("The number is Even");
+     }
+     else{
+        System.out.println("The number is Odd");
+     }
+    }
+}
+//boilerplate code
