a4905f953c0490dc1fc0784825bdc0d55cc4013f
Harshvardhan Chauhan <harshvardhan2266@gmail.com>
2025-08-07 18:51:22 +0530

    Pattern Advance

--- Patterns.java
+++ Patterns.java
@@ -1,4 +1,4 @@
-
+ 
 //Printing star pattern
 
 public class Patterns {
@@ -56,4 +56,168 @@ public class Patterns {
            System.out.println();
        }
     }
+}  
+
+// Printing Hollow Rectangle
+
+ public class Patterns {
+    public static void hollow_Rectangle(int totRows, int totCols) {
+        for(int i=1; i<=totRows; i++){
+            for(int j=1; j<=totCols; j++){
+                if(i == 1 || i == totRows || j == 1 || j == totCols){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        hollow_Rectangle(4, 5);
+    }
+}  
+
+//
+
+public class Patterns {
+    public static void inverted_rotated_half_pyramid(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                    System.out.print(" ");
+                }
+                for(int j=1; j<=i; j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    public static void main(String args[]){
+        inverted_rotated_half_pyramid(4);
+    }
+}  
+
+//Printing Floyd Triangle
+
+ 
+public class Patterns {
+    public static void floydTriangle(int n) {
+        int counter=1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++, counter++){
+                    System.out.print(counter+" ");
+                }
+                System.out.println();
+            }
+        }
+    public static void main(String args[]){
+        floydTriangle(5);
+    }
+}  
+
+//0-1 Triangle
+
+
+ public class Patterns {
+    public static void zeroOneTriangle(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                   int num = (i+j)%2;
+                   if(num == 1){
+                    System.out.print(1+" ");
+                   }
+                   else{
+                    System.out.print(0+" ");
+                   }
+                }
+                System.out.println();
+            }
+        }
+    public static void main(String args[]){
+        zeroOneTriangle(5);
+    }
+}  
+
+//ButterFly Pattern
+ 
+public class Patterns {
+    public static void butterFly(int n) {
+        //1st half
+        for(int i=1; i<=n; i++){
+            //stars - i
+            for(int j=1; j<=i; j++){
+                    System.out.print("*");
+                   }
+                   //spaces - 2*(n-i)
+                   for(int j=1; j<=2*(n-i); j++){
+                    System.out.print(" ");
+                   }
+                   //stars - i
+                    for(int j=1; j<=i; j++){
+                    System.out.print("*");
+                   }
+                   System.out.println();
+                }
+                //2nd half
+                for(int i=n; i>=1; i--){
+                     //stars - i
+            for(int j=1; j<=i; j++){
+                    System.out.print("*");
+                   }
+                   //spaces - 2*(n-i)
+                   for(int j=1; j<=2*(n-i); j++){
+                    System.out.print(" ");
+                   }
+                   //stars - i
+                    for(int j=1; j<=i; j++){
+                    System.out.print("*");
+                   }
+                   System.out.println();
+                }
+            }
+    public static void main(String args[]){
+        butterFly(10);
+    }
 } 
+
+//Solid Rhombus
+ 
+public class Patterns {
+    public static void solidRhombus(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=(n-i); j++){
+                 System.out.print(" ");
+                }
+            for(int j=1; j<=n; j++){
+                 System.out.print("*");
+                }
+            System.out.println();
+            }
+        }
+    public static void main(String args[]){
+    solidRhombus(10);
+    }
+}  
+
+//HollowRhombus
+
+
+ public class Patterns {
+    public static void HollowRhombus(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=(2*n-i); j++) {
+                 if((i+j)==6 || (i+j)==10)
+                    System.out.print("*");
+                else if((i==1)&&(j>n) || (i==n) && (j<n))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+             System.out.println();
+        }
+    }
+    public static void main(String args[]){
+    HollowRhombus(5);
+    }
+}  
